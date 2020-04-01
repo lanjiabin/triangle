ed18df5b8a68267c425457be2cfbdc143173495e
lanjiabin <1323457707@qq.com>
2020-04-01 09:59:20 +0800

    改成显示系统音量

--- app/src/main/java/com/lanjiabin/triangle/MainActivity.java
+++ app/src/main/java/com/lanjiabin/triangle/MainActivity.java
@@ -2,34 +2,58 @@ package com.lanjiabin.triangle;
 
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
-    private EditText mEdit;
-    private Button mButton;
     private Triangle mTriangle;
+    private AudioManager mAudioManager;
+    VolumeReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       initView();
-       OnClick();
+        initView();
     }
-    public void initView(){
+
+    public void initView() {
         setContentView(R.layout.activity_main);
-        mTriangle=findViewById(R.id.triangle);
-        mEdit=findViewById(R.id.edit);
-        mButton=findViewById(R.id.button);
+        mTriangle = findViewById(R.id.triangle);
+
+        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        double percent = (double) currentVolume / maxVolume;
+
+        mTriangle.setPercent(percent);
+
+        mReceiver = new VolumeReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.media.VOLUME_CHANGED_ACTION");
+        this.registerReceiver(mReceiver, filter);
     }
-    public void OnClick(){
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTriangle.setPercent(Double.parseDouble(mEdit.getText().toString()));
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    private class VolumeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals("android.media.VOLUME_CHANGED_ACTION")) {
+                mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                double percent = (double) currentVolume / maxVolume;
+                mTriangle.setPercent(percent);
             }
-        });
+        }
     }
 }
